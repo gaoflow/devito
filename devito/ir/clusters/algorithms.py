b93dff06750fa2a312a233288b254717282c53df
Fabio Luporini <fabio@devitocodes.com>
2026-04-23 13:11:47 +0100

    mpi: Fixup HaloTouch placement

--- devito/ir/clusters/algorithms.py
+++ devito/ir/clusters/algorithms.py
@@ -441,9 +441,10 @@ class HaloComms(Queue):
 
         d = prefix[-1].dim
 
-        # Construct a representation of the halo accesses
-        processed = []
-        for c in clusters:
+        processed = list(clusters)
+
+        # Construct HaloTouches and inject them into `processed`
+        for n, c in enumerate(clusters):
             if c.properties.is_sequential(d) or \
                c in seen:
                 continue
@@ -480,10 +481,17 @@ class HaloComms(Queue):
 
             halo_touch = c.rebuild(exprs=expr, ispace=ispace, properties=properties)
 
-            processed.append(halo_touch)
-            seen.update({halo_touch, c})
+            # Insert `halo_touch` at the top of the IterationSpace within which
+            # `c` is scheduled
+            index = 0
+            for i, c1 in reversed(list(enumerate(processed[:n]))):
+                if not c1.ispace.is_subset(c.ispace):
+                    index = i + 1
+                    break
+                index = i
+            processed.insert(index, halo_touch)
 
-        processed.extend(clusters)
+            seen.update({halo_touch, c})
 
         return processed
 
